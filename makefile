5869bb8c13d080d5f62c2623cdce519b137f42bb
Jayr Alencar <jayralencarpereira@gmail.com>
2020-05-14 00:13:13 -0300

    fixes

--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 corpus:
 	wget -P ./data/ "http://www.nilc.icmc.usp.br/nilc/tools/fapesp-corpora.tar.gz"
-	tar -xvzf  ./data/fapesp-corpora.tar -C data
+	tar -xvzf  ./data/fapesp-corpora.tar.gz -C data
 	tar -xvzf ./data/fapesp-corpora/corpora/pt.tgz -C data/fapesp-corpora/corpora/
 	python prepare_corpus.py
--- prepare_corpus.py
+++ prepare_corpus.py
@@ -3,8 +3,10 @@ from os.path import isfile, join
 dataset_path = './data/fapesp-corpora/corpora/pt/data'
 onlyfiles = [f for f in listdir(dataset_path) if isfile(join(dataset_path, f))]
 
+FILES_NUMBER = 10
+
 trainin_text = ''
-for file_ in onlyfiles[:10]:
+for file_ in onlyfiles[:FILES_NUMBER]:
     trainin_text += open(dataset_path+"/"+file_).read()
 
 training_corpus = open('./data/training_corpus.txt', 'w')
